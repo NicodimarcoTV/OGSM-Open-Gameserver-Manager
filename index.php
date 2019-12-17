67591fbc7e6314808b85936e427b061f95dfe697
Philipp <coolusaHD@users.noreply.github.com>
2019-12-17 18:08:35 +0100

    Upload

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html lang="de">
-<html>
+
 <head>
 
-<meta http-equiv="expires" content="0">
-<meta http-equiv="cache-control" content="no-cache">
 
 <meta charset="utf-8">
 
 <link type="text/css" href="css/button.css" rel="stylesheet" media="screen" >
 <link type="text/css" href="css/horizontalemenue.css" rel="stylesheet" media="screen" >
+<<<<<<< Updated upstream
 <Style>
 body {
     background: url("Hintergrund.jpg");
@@ -18,6 +17,12 @@ body {
     padding-top: 40px;
 }
 </Style>
+=======
+<link type="text/css" href="css/general.css" rel="stylesheet" media="screen" >
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>OGSM</title>
+
+>>>>>>> Stashed changes
 </head>
 <body>
 
@@ -25,7 +30,7 @@ body {
 
 <h1 style="color:#52ce1c;">Herzlich Willkommen bei OGSM-Open-Gameserver-Manager</h1><p>
 
-<br></br>
+<br>
 <ul>
   <li><a class="active" href="index.php">Home</a></li>
   <li><a class="horizontalemenue" href="php/install.php">Installations Menü</a></li>
@@ -52,7 +57,7 @@ body {
 function reboot() {
   var txt;
   var r = confirm("Wollen sie den Server wirklich Neustarten ?");
-  if (r == true) {
+  if (r === true) {
    window.location.href="php/action.php?Aktion=reboot";
   } else {
    window.location.href="index.php";
@@ -64,7 +69,7 @@ function reboot() {
 function shutdown() {
   var txt;
   var r = confirm("Wollen sie den Server wirklich Herunterfahren ?");
-  if (r == true) {
+  if (r === true) {
    window.location.href="php/action.php?Aktion=shutdown";
   } else {
    window.location.href="index.php";
