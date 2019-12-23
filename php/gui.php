4a3d930afee58368b371dcb1a6323aaf1261aa8d
Philipp <coolusaHD@users.noreply.github.com>
2019-12-23 21:39:04 +0100

    Update

--- php/gui.php
+++ php/gui.php
@@ -28,58 +28,134 @@ echo "$Name";
 <html>
 <head>
 
-<meta charset="utf-8">
+    <meta charset="utf-8">
 
-<link type="text/css" href="../css/button.css" rel="stylesheet" media="screen" >
-<link type="text/css" href="../css/horizontalemenue.css" rel="stylesheet" media="screen" >
-<Style>
-body {
-    background: url("../config/Hintergrund.jpg");
-    background-size:cover;
-    background-repeat: no-repeat;
-    padding-top: 40px;
-}
-</Style>
+    <link type="text/css" href="../css/button.css" rel="stylesheet" media="screen" >
+    <link type="text/css" href="../css/horizontalemenue.css" rel="stylesheet" media="screen" >
+
+    <link type="text/css" href="../css/general.css" rel="stylesheet" media="screen" >
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="/js/select.js"></script>
+    <title>OGSM</title>
+
+
+    <link type="text/css" href="../css/general.css" rel="stylesheet" media="screen" >
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<h1 style="color:#52ce1c;">Herzlich Willkommen bei OGSM-Open-Gameserver-Manager</h1><p>
+<<header>
+    <p><h1>
+        <span class="highlight">O</span>pen-<span class="highlight">G</span>ame<span class="highlight">S</span>erver-<span class="highlight">M</span>anager
+    </h1>
+</header>
+<p>
 
 <br></br>
 <ul>
-  <li><a class="active" href="../index.php">Home</a></li>
-  <li><a class="horizontalemenue" href="install.php">Installations Menü</a></li>
+    <li><a href="../index.php"><i class="fas fa-home"></i> Home</a></li>
+    <li><a href="install.php"><i class="fas fa-download"></i> Installations Menü</a></li>
+    <li><a class="active" href="gui.php"><i class="fas fa-terminal"></i> Konsole</a></li>
+    <li><a href="config.php"><i class="fas fa-cogs"></i> Einstellungen</a></li>
+    <li class="right"><a href="help.php"><i class="far fa-question-circle"></i> Hilfe</a></li>
+    <li class="right"><a href="contact.php"><i class="far fa-address-book"></i> Kontakt</a></li>
 
-<?php include("../config/servers.txt"); ?>
+    <?php include("../config/servers.txt"); ?>
 
 </ul>
 
-<br></br>
+-<br>
+<div class="row">
+
+    <div class="column left">
+
+        <div class="column side">
+            <h2>Host System Optionen</h2>
+
+            <a class="buttongruen" onclick="reboot()">
+                <i class="fas fa-redo"></i>
+                Server Neustarten
+            </a>
+            <p>
+                <a class="buttonrot" onclick="shutdown()">
+                    <i class="fas fa-power-off"></i>
+                    Server Herunterfahren
+                </a>
+
+                -        <br>
+        </div>
 
 
-<h2 style="color:#1a9cba;">Server Optionen</h2>
+        <div class="column side">
+            <h2>Aktueller Gameserver</h2>
 
-<a class="buttongruen" href="action.php?Aktion=start&Server=<?php echo htmlspecialchars($Server); ?>" methode="get"><?php echo htmlspecialchars($Name); ?> Starten</a>
+            <a class="buttongruen" href="action.php?Aktion=start&Server=<?php echo htmlspecialchars($server); ?>" methode="get">
+                <?php echo htmlspecialchars($Server); ?> <i class="fas fa-rocket"></i> Starten</a>
+            <a class="buttonorange" href="action.php?Aktion=restart&Server=<?php echo htmlspecialchars($server); ?>" methode="get">
+                <?php echo htmlspecialchars($Server); ?><i class="fas fa-redo"></i> Neustarten</a>
 
-<a class="buttonorange" href="action.php?Aktion=restart&Server=<?php echo htmlspecialchars($server); ?>" methode="get"><?php echo htmlspecialchars($Name); ?> Neustarten</a>
+            <a class="buttonrot" href="action.php?Aktion=stop&Server=<?php echo htmlspecialchars($server); ?>" methode="get">
+                <?php echo htmlspecialchars($Server); ?> <i class="fas fa-power-off"></i> Stoppen</a>
 
-<a class="buttonrot" href="action.php?Aktion=stop&Server=<?php echo htmlspecialchars($server); ?>" methode="get"><?php echo htmlspecialchars($Name); ?> Stoppen</a>
-
-<h2 style='color:<?php echo htmlspecialchars($Color); ?>;'><?php echo htmlspecialchars($Status); ?></h2>
-
-<br></br>
-
-
-<iframe height="600" width="1300" src="console.php?Server=<?php echo htmlspecialchars($server); ?>">
-</iframe>
-
-<br></br>
-
-<a class="buttonrot" href="action.php?Aktion=deinstall&Server=<?php echo htmlspecialchars($server); ?>" methode="get"><?php echo htmlspecialchars($Name); ?> Deinstallieren </a>
+            <br>
+            <br>
+            <br>
+            <p></p>
+            <a class="buttonrot" href="action.php?Aktion=deinstall&Server=<?php echo htmlspecialchars($server); ?>" methode="get">
+                <?php echo htmlspecialchars($Server); ?><i class="fas fa-trash-alt"></i> Deinstallieren </a>
+            <br>
+            <br>
 
 
-</body>
-</html>
+        </div>
+    </div>
 
+    <div class="column spacer"></div>
+
+    <div class="column middle">
+        <h1>Graphical User Interface</h1>
+        <br>
+        <h4><p style="text-align: center"><strong>Interface:</strong></p></h4>
+
+        <iframe height="100%" width="100%" src="../Codiad/index.php">
+        </iframe>
+
+
+        <br>
+        <br>
+    </div>
+
+</div>
+
+<footer>
+    <p></p>
+    <p>©OGSM by NicodimarcoTV & CoolusaHD</p>
+</footer>
+
+
+
+<script>
+    function reboot() {
+        var txt;
+        var r = confirm("Wollen sie den Server wirklich Neustarten ?");
+        if (r === true) {
+            window.location.href="./action.php?Aktion=reboot";
+        } else {
+            window.location.href="./gui.php";
+        }
+    }
+</script>
+
+<script>
+    function shutdown() {
+        var txt;
+        var r = confirm("Wollen sie den Server wirklich Herunterfahren ?");
+        if (r === true) {
+            window.location.href="./action.php?Aktion=shutdown";
+        } else {
+            window.location.href="./gui.php";
+        }
+    }
+</script>
